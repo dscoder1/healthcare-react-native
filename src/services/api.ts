6e6145913f4211abd7769551ec8ba39c0c214126
Divya Shakti <166431272+dscoder1@users.noreply.github.com>
2026-08-04 23:10:24 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // CHANGE THIS IP
 
-const API_URL = "http://192.168.21.187:5000/api";
+const API_URL = "https://healthcare-backend-jljo.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
